74d2d30cc817b2c24f6fecbc7dac5e4e4fd06d68
Michael Bernat <michael@hibernat.com>
2022-06-12 22:09:07 +0200

    Initial Commit

---
+++ TCADemo/TCADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemoApp.swift
+//  TCADemo
+//
+//  Created by Michael Bernat on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TCADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
