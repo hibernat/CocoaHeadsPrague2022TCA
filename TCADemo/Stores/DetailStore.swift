1dac8d4c57d70a974a18fb744d7dee7a4a8cffbe
Michael Bernat <michael@hibernat.com>
2022-06-14 20:10:48 +0200

    DetailView + DetailStore

---
+++ TCADemo/Stores/DetailStore.swift
@@ -0,0 +1,39 @@
+//
+//  DetailStore.swift
+//  TCADemo
+//
+//  Created by Michael Bernat on 14.06.2022.
+//
+
+import Foundation
+import ComposableArchitecture
+
+//MARK: - DetailState
+struct DetailState: Equatable {
+    
+    @BindableState var sliderValue: Double
+    
+}
+
+//MARK: - DetailAction
+enum DetailAction: BindableAction {
+    case binding(BindingAction<DetailState>)
+}
+
+//MARK: - DetailEnvironment
+struct DetailEnvironment {
+    
+}
+
+//MARK: - DetailState reducer
+extension DetailState {
+    
+    static let reducer: Reducer<DetailState, DetailAction, DetailEnvironment> = .init { state, action, environment in
+        
+        return .none
+        
+    }
+    .binding()
+    
+}
+
---
+++ TCADemo/Views/DetailView.swift
@@ -0,0 +1,42 @@
+//
+//  DetailView.swift
+//  TCADemo
+//
+//  Created by Michael Bernat on 14.06.2022.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct DetailView: View {
+    
+    let store: Store<DetailState, DetailAction>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            Form {
+                VStack(alignment: .leading) {
+                    Text("Slider value: \(Int(viewStore.sliderValue))")
+                    HStack {
+                        Text("1")
+                        Slider(value: viewStore.binding(\.$sliderValue), in: 1...100)
+                        Text("100")
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(
+            store: .init(
+                initialState: .init(sliderValue: 50),
+                reducer: DetailState.reducer,
+                environment: .init()
+            )
+        )
+    }
+}
